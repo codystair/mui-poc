168dc8fc20f84c655df449914b4048d571cd0e94
Cody Stair <ccstair27@gmail.com>
2021-05-22 14:02:54 -0400

    basic setup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,15 @@
-import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { CssBaseline, ThemeProvider } from "@material-ui/core";
+import { theme } from "../src";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={theme}>
+    {/* CssBaseline kickstart an elegant, consistent, and simple baseline
+    to build upon. */}
+    <CssBaseline />
+    <Component {...pageProps} />
+  </ThemeProvider>
+  )
 }
 export default MyApp
---
+++ src/theme.ts
@@ -0,0 +1,17 @@
+import { createMuiTheme } from '@material-ui/core'
+
+export const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#0b72b9',
+    },
+    secondary: {
+      main: '#ffba60',
+    },
+  },
+  typography: {
+    h3: {
+      fontWeight: 300,
+    },
+  },
+})
